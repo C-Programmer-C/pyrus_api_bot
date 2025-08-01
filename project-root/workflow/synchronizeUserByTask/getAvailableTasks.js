29b85f6975b5fc048f0ee28fe75bc6a3104b8b51
C-Programmer-C <mihailumov5@gmail.com>
2025-08-01 13:36:42 +0500

    Update getAvailableTasks.js

--- project-root/workflow/synchronizeUserByTask/getAvailableTasks.js
+++ project-root/workflow/synchronizeUserByTask/getAvailableTasks.js
@@ -3,10 +3,12 @@ import {getTasksByChannel} from '../taskUtils.js';
 export async function getAvailableTasks(api) {
     const EMAIL_CHANNEL_CHOICE_ID = 2;
     const TELEPHONE_CHANNEL_CHOICE_ID = 1;
-
+    const MESSENGER_CHANNEL_CHOICE_ID = 3;
+    
     const emailTasks = await getTasksByChannel(api, EMAIL_CHANNEL_CHOICE_ID, false);
     const telephoneTasks = await getTasksByChannel(api, TELEPHONE_CHANNEL_CHOICE_ID, false);
+    const messengerTasks = await getTasksByChannel(api, MESSENGER_CHANNEL_CHOICE_ID, false);
 
-    return emailTasks.concat(telephoneTasks);
+    return emailTasks.concat(telephoneTasks).concat(messengerTasks);
     
 }
